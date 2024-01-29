bbb6da7e994f7f23954a714cc38b6f997fb2a7ca
Kostia Marchuk <121522005+kostomeister@users.noreply.github.com>
2024-01-30 01:48:22 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import time
+
 from bingchat import BingChat
 
 chat = BingChat()
@@ -6,6 +8,6 @@ chat.set_tone_creative()
 
 chat.set_prompt("Describe this image.")
 chat.upload_image("example.jpg")
-
+time.sleep(1)
 response = chat.submit()
 print(response)
